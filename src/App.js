c55793a01741b92b19fec56c5b8e81de463ed4cd
AJuskys <81487998+AJuskys@users.noreply.github.com>
2022-06-30 17:51:53 +0100

    Adding - React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>New Start</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
